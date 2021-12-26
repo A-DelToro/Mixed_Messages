1e70ddd9e8c866993d9dfc1281f3ec8e113cffd9
Adam Del Toro <96677378+A-DelToro@users.noreply.github.com>
2021-12-26 03:57:41 -0700

    Adds new random messahe

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -1,3 +1,8 @@
-let adjArr = ['Ugly', 'Weird', 'Swetty', 'Cheesy'];
-let nounArr = ['Girl', 'Boy', 'Cow', 'Dog'];
-console.log(`You are a ${adjArr[0]} ${nounArr[0]}`);
+let gender = ['Male', 'Female']
+let race = ['Orc', 'Elf', 'Human', 'Cat', 'Rat', 'Dragonkin']
+let role = ['Archer', 'Wizard', 'Warrior', 'Warlock', 'Ninja', 'Thief', 'Peasent', 'Priest', 'Shaman']
+let randomG = Math.floor(Math.random() * gender.length);
+let randomR = Math.floor(Math.random() * race.length);
+let randomRO = Math.floor(Math.random() * role.length);
+
+console.log(`You are a ${gender[randomG]} ${race[randomR]}. Your Role is ${role[randomRO]}`);
